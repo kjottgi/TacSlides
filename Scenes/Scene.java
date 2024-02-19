886618d4db1beb5289bbd36f12d2f268eadc35f5
Jott <karmjotgirn@gmail.com>
2024-02-19 16:30:41 -0500

    added some classes

---
+++ Scenes/Scene.java
@@ -0,0 +1,19 @@
+/**
+ * Class Frame.
+ *
+ * Basic Frame Class to encapsulate Frames
+ *
+ *
+ */
+public class Scene
+{
+    ArrayList frames = new Frames(); //the frames stored in this scene
+
+
+
+
+
+
+
+
+}
