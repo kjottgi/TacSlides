b3183c06a5a2d6074aadd268efd6a4e4c01f159e
Jott <karmjotgirn@gmail.com>
2024-02-19 16:37:46 -0500

    some basic class stuff

---
+++ Scenes/DialogueScene.java
@@ -0,0 +1,9 @@
+public class DialogueScene extends Scene
+{
+
+    public DialogueScene(){}
+
+
+
+
+}
--- Scenes/Scene.java
+++ Scenes/Scene.java
@@ -5,12 +5,13 @@
  *
  *
  */
-public class Scene
+public abstract class Scene
 {
-    ArrayList frames = new Frames(); //the frames stored in this scene
+    private ArrayList frames = new ArrayList<Frame>; //the frames stored in this scene
+    public static number_of_scenes = 0; //the number of scenes made
+    private name; //The name of the current scene.
 
-
-
+    public Scene(){this.number_of_scenes = this.number_of_scanes++;}
 
 
 
